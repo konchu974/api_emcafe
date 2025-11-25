e193a8e72bfc52e8c013adf7810132606021917f
konchu974 <dugain.c@outlook.com>
2025-11-25 10:58:14 +0100

    Add compiled files for Render

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,16 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'emca_bdd',
-  synchronize: false,
-  logging: process.env.NODE_ENV === 'development',
+  host: 'caboose.proxy.rlwy.net',
+  port: 26663,
+  username: 'root',
+  password: 'ICSKMyDWkTsVEOLwYCiFCdUbiRJxLHOV',
+  database: 'railway',
+  extra: {
+    allowPublicKeyRetrieval: true,
+    ssl: false // si pas de SSL
+  },
   entities: ['src/entities/**/*.ts'],
-  migrations: ['src/migrations/**/*.ts'],
-  subscribers: [],
+  synchronize: true,
 });
